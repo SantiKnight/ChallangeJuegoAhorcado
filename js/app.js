c52fcdcf118db22cfd221e5d554ce206c63493c4
Santi Knight <santiknight2003@gmail.com>
2022-05-27 13:22:08 -0300

    Codificar función de transición

--- js/app.js
+++ js/app.js
@@ -15,11 +15,14 @@
     function dibujar(juego){
         let $elem
         $elem = $html.hombre
-        $elem.src = 'imagenes/0' + juego.estado + '.png'
+        let estado = juego.estado
+        $elem.src = 'imagenes/0' + estado + '.png'
 
         let palabra = juego.palabra
         let adivinado = juego.adivinado
         $elem = $html.adivinado
+        //Borramos los elementos anteriores
+        $elem.innerHTML = ''
         for(let letra of palabra){
             let $span = document.createElement('span')
             let $txt = document.createTextNode('')
@@ -33,6 +36,8 @@
 
         let errado = juego.errado
         $elem = $html.errado
+        //Borramos los elementos anteriores
+        $elem.innerHTML = ''
         for (let letra of errado){
             let $span = document.createElement('span')
             let $txt = document.createTextNode(letra)
@@ -42,6 +47,59 @@
         }
     }
 
+
+    function adivinar(juego, letra){
+        let estado = juego.estado
+        // si ya se a perdido o ganado, nada que hacer
+        if (estado===0 || estado===8){
+            return
+        }
+
+        let adivinado = juego.adivinado
+        let errado = juego.errado
+        // Si ya hemos adivinado o errado la letra, tampoco  hacemos nada
+        if(adivinado.indexOf(letra) >= 0 || errado.indexOf(letra) >= 0){
+            return
+        }
+
+        let palabra = juego.palabra
+        //si es letra de la palabra
+        if(palabra.indexOf(letra) >= 0){
+            let ganado = true;
+            //debemos ver si llegamos al estado de ganado
+            for(let l of palabra){
+                if(adivinado.indexOf(l) < 0 && l != letra){
+                    ganado = false
+                    juego.previo = juego.estado
+                    break
+                }
+            }
+            //si ya se ha ganado debemos indicarlo 
+            if(ganado){
+                alert('Felicitaciones! Usted ganó')
+            }
+            //agregar la letra a la lista de letras adivinadas
+            adivinado.push(letra)
+        }
+        else{
+            //si no es letra de la palabra, acercamos al hombre un paso mas a ser ahorcado y agregamos la letr a las letras erradas
+            juego.estado++
+            errado.push(letra)
+        }
+    }
+
+    window.onkeypress = function adivinarLetra(e){
+        let letra = e.key
+        letra = letra.toUpperCase()
+        if(/[A-ZÑ]/.test(letra)){
+            adivinar(juego,letra)
+            dibujar(juego)
+        }
+        else{
+            alert('Solo letras')
+        }
+        
+    }
 
     dibujar(juego)
 }())
