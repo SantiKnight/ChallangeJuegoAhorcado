3a09c7e83786a56273b818ac572ae536338f9efd
Santi Knight <santiknight2003@gmail.com>
2022-05-30 19:57:44 -0300

    Cambio en el script para que al agregar una palabra esta se borre al querer ingresar una nueva

--- js/bienvenida.js
+++ js/bienvenida.js
@@ -2,18 +2,19 @@
 let bienvenida = document.querySelector('.bienvenida');
 let principal = document.querySelector('.juego-ahorcado');
 let agregar = document.querySelector('.agregar-palabra');
+let input = document.getElementById('input-text')
 
 function iniciarJuego(){
     bienvenida.classList.add("invisible");
     agregar.classList.add("invisible");
     principal.classList.remove("invisible");
-    
 }
 
 function ocultar(){
     bienvenida.classList.add("invisible");
     principal.classList.add("invisible");
     agregar.classList.remove("invisible");
+    input.value = "";
 }
 
 function cancelar(){
